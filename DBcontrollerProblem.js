4fb864aecbd512c6f386b19b18ae73a08d777211
MathewJoseph31 <mathew_joseph31@yahoo.com>
2018-12-29 09:44:58 -0500

    Edit problem window added

--- DBcontrollerProblem.js
+++ DBcontrollerProblem.js
@@ -22,6 +22,7 @@ app.use(session({secret:'secr3',resave:false,saveUninitialized:false, maxAge: 24
 const url = require('url');
 
 const configuration=require('./Configuration');
+const dbControllerQuiz=require('./DBcontrollerQuiz');
 
 //----PROBLEM----
 
@@ -73,7 +74,8 @@ exports.displayProblems=function(req,res){
     ssl:true
   });
 
-  var sql = "select distinct A.description, A.solution, B.description quiz_description from Problem A inner join Quiz B on A.quiz_id=B.id";
+  var sql = "select distinct A.id, A.description, A.option1, A.option2, A.option3, A.option4, answerkey, "+
+   "A.solution, A.type, A.author_id, A.quiz_id, B.description quiz_description from Problem A inner join Quiz B on A.quiz_id=B.id";
 
   pool.query(sql, function (err, result, fields){
     if (err) throw err;
@@ -93,16 +95,68 @@ exports.displayProblems=function(req,res){
     str+='<a class="HomeLink" href="/">back to home</a>'+
     '<div class="h1">'+
     'View Problems'+
-    '</div>';
+    '</div>\
+    <div id="updateWindow" class="updateWindow">\
+    <button id = "x" onclick="closeUpdateWindow()">\
+              X\
+    </button>\
+    <fieldset>\
+        <br>\
+      Quiz Id:<br>';
 
+      var quizList=dbControllerQuiz.getQuizListSync();
+        str+='<select id="quizId" name="quizId" required="true">';
+         for(var i=0;i<quizList.length;i++){
+            var index=quizList[i].indexOf('$,');
+            var valQuiz=quizList[i].substring(index+2);
+            var textQuiz=quizList[i].substring(0,index);
+            console.log(valQuiz+' , '+textQuiz);
+            str+='<option value="'+valQuiz+'">'+textQuiz+'</option>';
+           }
+        str+='</select> <br>';
+
+      str+='<!--<input id="courseId" name="courseId" type="text" cols="150" required="true"></input> <br>-->\
+    Problem Description:<br>\
+    <textarea id="probDescription" name="probDescription" type="text" rows="10" cols="150" required="true">\
+    </textarea>\
+    <br>\
+    <br>\
+    Answer Description:<br>\
+    <textarea id="ansDescription" name="ansDescription" type="text" rows="10" cols="150" required="true">\
+    </textarea> <br>\
+    <br>\
+    <br>\
+    Author:<br>\
+    <input id="authorName" name="authorName" type="text" cols="150" required="true">\
+    </input> <br>\
+    <br>\
+    <input type="button" value="Save Updates"/>\
+    </div>';
     var i=0;
     for(i=0;i<result.rows.length;i++){
-      str=str+'<b>Quiz: </b><div class="Quiz">'+result.rows[i].quiz_description+'</div></br></br>';
+      str+='<hr><div id="par'+result.rows[i].id+'" class="probParent">';
+      if(req.session.userId){
+        /* replace quotes and double quotes with corresponding html sequences*/
+          var desc=result.rows[i].description;
+          desc=desc.replace(/\"/g,"&quot;");
+          desc=desc.replace(/'/g,"&#39;");
+          var sol=result.rows[i].solution;
+          sol=sol.replace(/\"/g,"&quot;");
+          sol=sol.replace(/'/g,"&#39;");
+          str+='<input type="button" class="EditButton" onclick="editProblemHandler(\''+
+          result.rows[i].id+'\', \''+desc+'\', \''+result.rows[i].option1+'\', \''+
+          result.rows[i].option2+'\',\''+result.rows[i].option3+'\',\''+result.rows[i].option4+'\','+
+          result.rows[i].answerkey+',\''+sol+'\',\''+result.rows[i].author_id+'\',\''+
+          result.rows[i].quiz_id+'\')" id="d'+result.rows[i].id+'" value="Edit Problem"/>';
+      }
+      str=str+'<b>Quiz: </b><div class="Quiz">'+result.rows[i].quiz_description+'</div>';
+      str+='</br></br>';
       str=str+'<b>Question: </b><div class="Question">'+result.rows[i].description+'</div>'+
       '<input type="button" class="showAnswer" onclick="showAnswerHandler(this)" id="b'+i+'$" value="view solution"/></br>' +
-      '<div id="d'+i+'" class="Answer"><b>Solution: </b>'+result.rows[i].solution+'</div><hr>';
+      '<div id="d'+i+'" class="Answer"><b>Solution: </b>'+result.rows[i].solution+'</div>';
+      str+='</div><!--end of par div-->'
     }
-    str=str+'</body>'+
+    str=str+'<hr></body>'+
     '<script type="text/javascript" src="scripts/general.js">'+
     '</script>' +
     '<script type="text/javascript" src="scripts/problem.js">'+
--- public/scripts/problem.js
+++ public/scripts/problem.js
@@ -1,4 +1,33 @@
 document.body.onload=changeHandler;
+
+function closeUpdateWindow(){
+  var updateDiv=document.getElementById("updateWindow");
+  updateDiv.style.display='none';
+}
+
+
+function editProblemHandler(id, description, option1, option2, option3, option4,
+ answerkey, solution, author_id, quiz_id){
+
+ //alert(''+id+' '+description+' \n '+solution+' '+option3+' '+option4+' '+answerkey+' '+author_id+' '+quiz_id);
+ var updateDiv=document.getElementById("updateWindow");
+ updateDiv.style.display='block';
+
+ document.getElementById('probDescription').value=description;
+ document.getElementById('ansDescription').value=solution;
+ document.getElementById('authorName').value=author_id;
+
+  /*var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+
+      updateDiv.innerHTML = this.responseText;
+
+    }
+  };
+  xhttp.open("GET", "editProblem?id="+problemId, true);
+  xhttp.send();*/
+}
 /*
 document.getElementById('showButton').onclick=makeVisible;
 
