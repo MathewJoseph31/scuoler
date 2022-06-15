cb02221a88c9a448e029edaa7865c3a84a5dbcc6
MathewJoseph31 <mathew_joseph31@yahoo.com>
2022-06-15 12:55:30 -0400

    recording option

--- public/scripts/videoChat.js
+++ public/scripts/videoChat.js
@@ -18,6 +18,8 @@ const peers = {};
 
 let peer;
 let screenShare = false;
+let recording = false;
+let myMediaRecorder;
 
 const getPeer = () => {
   if (!peer) {
@@ -321,6 +323,8 @@ const setPlayVideoButton = () => {
   videoEle.innerHTML = html;
 };
 
+//////////////////////Screen share///////////////////////////////
+
 const switchToScreenShare = async () => {
   let localStream = await getMyVideoStream();
   if (!screenShare) {
@@ -377,6 +381,95 @@ const replaceVideoTracks = async (oldStream, newStream) => {
       await oldStream.addTrack(newStream.getTracks()[i]);
   }
 };
+///////////End of Screenshare area ///////////////
+
+///////////Record Video area ///////////////
+
+const saveFile = (recordedChunks) => {
+  const blob = new Blob(recordedChunks, {
+    type: "video/webm",
+  });
+
+  let filename = window.prompt("Enter file name"),
+    downloadLink = document.createElement("a");
+  downloadLink.href = URL.createObjectURL(blob);
+  downloadLink.download = `${filename}.webm`;
+
+  document.body.appendChild(downloadLink);
+  downloadLink.click();
+  URL.revokeObjectURL(blob); // clear from memory
+  document.body.removeChild(downloadLink);
+};
+
+const createRecorder = (stream, mimeType) => {
+  // the stream data is stored in this array
+  let recordedChunks = [];
+
+  const mediaRecorder = new MediaRecorder(stream);
+
+  mediaRecorder.ondataavailable = function (e) {
+    if (e.data.size > 0) {
+      recordedChunks.push(e.data);
+    }
+  };
+  mediaRecorder.onstop = function () {
+    saveFile(recordedChunks);
+    recordedChunks = [];
+  };
+  mediaRecorder.start(200); // For every 200ms the stream data will be stored in a separate chunk.
+  return mediaRecorder;
+};
+
+const addAudioTracks = async (screenStream) => {
+  let myStream = await getMyVideoStream();
+
+  let audioTracksArr = [...myStream.getAudioTracks()];
+  for (let i = 0; i < audioTracksArr.length; i++)
+    await screenStream.addTrack(audioTracksArr[i]);
+};
+
+const startRecord = async () => {
+  if (!recording) {
+    let recordStream;
+    if (!screenShare) {
+      recordStream = await window.navigator.mediaDevices.getDisplayMedia(
+        camMediaOptions
+      );
+      await addAudioTracks(recordStream);
+    } else {
+      recordStream = await getMyVideoStream();
+    }
+    let mimeType = "video/webm";
+    myMediaRecorder = createRecorder(recordStream, mimeType);
+
+    recording = true;
+    setRecordButton();
+  } else {
+    myMediaRecorder.stop();
+    recording = false;
+    setNoRecordButton();
+  }
+};
+
+const setRecordButton = () => {
+  const html = `<i class="record fas fa-record-vinyl"></i>
+                <span>Record</span>`;
+  const recordEle = document.getElementsByClassName(
+    "videoChat__left__record__button"
+  )[0];
+  recordEle.innerHTML = html;
+};
+
+const setNoRecordButton = () => {
+  const html = `<i class="noRecord fas fa-record-vinyl"></i>
+                <span>Record</span>`;
+  const recordEle = document.getElementsByClassName(
+    "videoChat__left__record__button"
+  )[0];
+  recordEle.innerHTML = html;
+};
+
+/////////////End of record Video area //////////////
 
 const leaveMeeting = () => {
   if (window.confirm("Do you really want to leave the Meeting?")) {
