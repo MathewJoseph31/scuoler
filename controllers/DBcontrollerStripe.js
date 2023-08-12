a36ad7e048d7ecdf6ffea247cb302960ec700a90
mathew joseph <mathew_joseph31@yahoo.com>
2023-08-12 09:46:05 -0500

    error handling

--- controllers/DBcontrollerStripe.js
+++ controllers/DBcontrollerStripe.js
@@ -11,30 +11,36 @@ exports.stripeSessionCheckout = async (req, res, next) => {
   let productDescription = req.body.productDescription;
   let productUrl = req.body.productUrl;
   let productPrice = req.body.productPrice;
-  let product = await stripe.products.create({
-    name: productName,
-    description: productDescription,
-    images: [productUrl],
-  });
-  //console.log(product);
-  const price = await stripe.prices.create({
-    product: product.id,
-    unit_amount: productPrice * 100,
-    currency: "usd",
-  });
-  console.log(price);
-  //let referrer = req.headers.referrer || req.headers.referer;
-  const url =
-    req.headers["x-forwarded-proto"] + "://" + req.headers["x-forwarded-host"];
-  console.log(url);
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
-    line_items: [{ price: price.id, quantity: 1 }],
-    mode: "payment",
-    success_url: `${url}/donateSuccess?session_id={CHECKOUT_SESSION_ID}`,
-    cancel_url: `${url}/`,
-  });
-  console.log(session);
-  setCorsHeaders(req, res);
-  res.json({ checkoutstatus: "ok", session: session.id });
+  try {
+    let product = await stripe.products.create({
+      name: productName,
+      description: productDescription,
+      images: [productUrl],
+    });
+    //console.log(product);
+    const price = await stripe.prices.create({
+      product: product.id,
+      unit_amount: productPrice * 100,
+      currency: "usd",
+    });
+    console.log(price);
+    //let referrer = req.headers.referrer || req.headers.referer;
+    const url =
+      req.headers["x-forwarded-proto"] +
+      "://" +
+      req.headers["x-forwarded-host"];
+    console.log(url);
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: [{ price: price.id, quantity: 1 }],
+      mode: "payment",
+      success_url: `${url}/donateSuccess?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${url}/`,
+    });
+    console.log(session);
+    setCorsHeaders(req, res);
+    res.json({ checkoutstatus: "ok", session: session.id });
+  } catch (e) {
+    res.json({ checkoutstatus: "error", message: e.toString() });
+  }
 };
