7c780f533359d9e69f78c9b14084eb27d3f7a4cb
mathew joseph <mathew_joseph31@yahoo.com>
2024-06-22 00:24:52 +0530

    new client build added in client_build dir

---
+++ client_build/precache-manifest.7de40ce81315bc6d7cfcb92c01be9485.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61284cd7c418f5fba936ac42b70b4fa5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6aa98410d678a8608061",
+    "url": "/static/css/2.274f5c1b.chunk.css"
+  },
+  {
+    "revision": "d4e23706ad3d6a18bcc2",
+    "url": "/static/css/main.c59cb3a9.chunk.css"
+  },
+  {
+    "revision": "6aa98410d678a8608061",
+    "url": "/static/js/2.09b3831f.chunk.js"
+  },
+  {
+    "revision": "b9c148c2c43afaf1238d815620e9a229",
+    "url": "/static/js/2.09b3831f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4e23706ad3d6a18bcc2",
+    "url": "/static/js/main.50a9d697.chunk.js"
+  },
+  {
+    "revision": "7cf1e237e5372a2442180c84c5640bb3",
+    "url": "/static/js/main.50a9d697.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d37933fada7fe1a39817",
+    "url": "/static/js/runtime-main.c564f432.js"
+  },
+  {
+    "revision": "1888daf827a194f86c6644ae0a76b17c",
+    "url": "/static/media/Corporate-LMS-Vision.1888daf8.webp"
+  },
+  {
+    "revision": "a374cadd70cc5ef17ae84ca12a33b16f",
+    "url": "/static/media/Corporate-Learning-management-system-benefits.a374cadd.jpg"
+  },
+  {
+    "revision": "63f7ca4e64ec9707916896fd8b3cbf64",
+    "url": "/static/media/aboutus_banner.63f7ca4e.jpg"
+  },
+  {
+    "revision": "ef1ef29fad0bb84fcbcc8f7b94994ad4",
+    "url": "/static/media/aboutus_phil.ef1ef29f.jpg"
+  },
+  {
+    "revision": "f20b13ee8bd05ce944cd4bb8bb0810d8",
+    "url": "/static/media/corporateLmsBenefits.f20b13ee.png"
+  },
+  {
+    "revision": "b65dabefdff280461e997947a872568d",
+    "url": "/static/media/create1.b65dabef.png"
+  },
+  {
+    "revision": "27534d6a00213b53ce34c76983b275be",
+    "url": "/static/media/how-to-study-online-featured-image.27534d6a.jpg"
+  },
+  {
+    "revision": "ab44670590444693717cc8ec5bf312a1",
+    "url": "/static/media/organize.ab446705.png"
+  },
+  {
+    "revision": "3954f712a1654242b8602531dad80524",
+    "url": "/static/media/scuoler_logo.3954f712.svg"
+  },
+  {
+    "revision": "37108d91b843611139d9573a50a93e69",
+    "url": "/static/media/scuoler_logo_white.37108d91.svg"
+  },
+  {
+    "revision": "d7125cd4a624530ada360d16605527c0",
+    "url": "/static/media/why-build-your-own-corporate-lms.d7125cd4.jpeg"
+  }
+]);
