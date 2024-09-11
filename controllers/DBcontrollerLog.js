80113655130c1eba616f4827d7ac678d141b6b20
mathew joseph <mathew_joseph31@yahoo.com>
2024-09-11 09:04:43 +0530

    filter bot flag added to getLogs API

--- controllers/DBcontrollerLog.js
+++ controllers/DBcontrollerLog.js
@@ -17,13 +17,14 @@ exports.getWebLogs = async function (req, res, next) {
   var queryObject = url.parse(req.url, true).query;
   let startDateTime = queryObject.startDate;
   let endDateTime = queryObject.endDate;
+  let filterBots = queryObject.filterBots;
 
   let dt_startDateTime_utc = new Date(startDateTime);
   let dt_endDateTime_utc = new Date(endDateTime);
 
   let pageSize = queryObject.pageSize || 500;
   let currentPage = queryObject.currentPage || 1;
-  //console.log(pageSize+', currPage '+currentPage);
+  // console.log(filterBots + ", currPage " + currentPage);
 
   const offset = pageSize * (currentPage - 1);
 
@@ -57,15 +58,11 @@ exports.getWebLogs = async function (req, res, next) {
           referrer,
           user_agent,
           log_filename 
-    from web_logs 
-    where log_timestamp>= $1 and log_timestamp< $2
-    order by log_timestamp
-    limit $3 offset $4
+    from web_logs_get(p_startDateTime:=$1, p_endDateTime:=$2, p_filterBot:=$3, p_offset:=$4, p_limit:=$5) 
       `;
-
   pool.query(
     sql,
-    [dt_startDateTime_utc, dt_endDateTime_utc, pageSize, offset],
+    [dt_startDateTime_utc, dt_endDateTime_utc, filterBots, offset, pageSize],
     async function (err, result, fields) {
       pool.end(() => {});
       if (err) next(err);
