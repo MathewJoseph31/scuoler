1a99244f9d499b20101338971955d11f597c02ab
mathew joseph <mathew_joseph31@yahoo.com>
2024-10-26 09:29:56 +0530

    Negative marking feature

--- controllers/DBcontrollerProblem.js
+++ controllers/DBcontrollerProblem.js
@@ -120,6 +120,8 @@ exports.insertProblemToDbJson = async function (req, res, next) {
   let answerKey = req.body.answerKey;
   let ansDescription = req.body.ansDescription;
   let authorName = req.body.authorName;
+  let maximumMarks = req.body.maximumMarks;
+  let negativeMarks = req.body.negativeMarks;
 
   if (answerKey === "") {
     answerKey = -1;
@@ -163,7 +165,8 @@ exports.insertProblemToDbJson = async function (req, res, next) {
   var sql =
     "select problem_insert(p_id :=$1, p_description:=$2, p_options:=$3, " +
     "  p_solution:=$4, p_answerkey:=$5, p_author_id:=$6, p_quizes_id:=$7, " +
-    " p_categories_id:=$8, p_type:=$9, p_problem_open:=$10, p_solution_open:=$11);";
+    ` p_categories_id:=$8, p_type:=$9, p_problem_open:=$10, p_solution_open:=$11, 
+    p_maxmarks:=$12, p_negative_marks:=$13);`;
 
   var problemId = utils.getUniqueId(authorName);
 
@@ -181,6 +184,8 @@ exports.insertProblemToDbJson = async function (req, res, next) {
       problemType,
       problemOpen,
       solutionOpen,
+      maximumMarks,
+      negativeMarks,
     ],
     function (err, result) {
       pool.end(() => {});
@@ -204,15 +209,14 @@ exports.editProblemInDB = async function (req, res, next) {
   let solutionOpen = req.body.solutionOpen;
 
   let options = JSON.parse(req.body.options);
-  /*let option1 = req.body.option1;
-  let option2 = req.body.option2;
-  let option3 = req.body.option3;
-  let option4 = req.body.option4;*/
   let answerkey = req.body.answerkey;
   let quizId = req.body.quizId;
   let quizesArray = JSON.parse(req.body.quizesArray);
-  var solution = req.body.solution;
-  var authorId = req.body.authorId;
+  let solution = req.body.solution;
+  let authorId = req.body.authorId;
+
+  let maximumMarks = req.body.maximumMarks;
+  let negativeMarks = req.body.negativeMarks;
 
   let categoriesArray = [];
 
@@ -235,9 +239,9 @@ exports.editProblemInDB = async function (req, res, next) {
     quizesId.push(item.id);
   });
 
-  var sql =
-    "select problem_update(p_id:=$1,p_description:=$2,p_options:=$3, " +
-    " p_solution:=$4, p_answerkey:=$5, p_quizes_id:=$6, p_categories_id:=$7, p_type:=$8, p_problem_open:=$9, p_solution_open:=$10);";
+  var sql = `select problem_update(p_id:=$1,p_description:=$2,p_options:=$3, 
+     p_solution:=$4, p_answerkey:=$5, p_quizes_id:=$6, p_categories_id:=$7, p_type:=$8, 
+     p_problem_open:=$9, p_solution_open:=$10, p_maxmarks:=$11, p_negative_marks:=$12);`;
 
   let accountId = req.body.accountId;
   let accountConfiguration = configuration;
@@ -271,6 +275,8 @@ exports.editProblemInDB = async function (req, res, next) {
       type,
       problemOpen,
       solutionOpen,
+      maximumMarks,
+      negativeMarks,
     ],
     function (err, result, fields) {
       pool.end(() => {});
@@ -382,6 +388,8 @@ exports.getTheProblem = async function (req, res, next) {
         resObj.source = result.rows[0].source;
         resObj.problem_open = result.rows[0].problem_open;
         resObj.solution_open = result.rows[0].solution_open;
+        resObj.maxmarks = result.rows[0].maxmarks;
+        resObj.negative_marks = result.rows[0].negative_marks;
         resObj.view_count = result.rows[0].view_count;
         resObj.rating = result.rows[0].rating;
         resObj.likes = result.rows[0].likes;
@@ -445,10 +453,10 @@ exports.getProblems = async function (req, res, next) {
     ssl: { rejectUnauthorized: false },
   });
 
-  var sql =
-    "select A.id,  A.description, A.options,  A.answerkey, " +
-    " A.solution, A.type, A.problem_open, A.solution_open, A.author_id, A.source, A.author_name, A.view_count " +
-    " from problem_get_all(p_category:=$1, p_language:=$2, p_sort:=$3, p_author:=$4, p_author_filter:=$5, p_offset:=$6, p_limit:=$7) A ";
+  var sql = `select A.id,  A.description, A.options,  A.answerkey, 
+     A.solution, A.type, A.problem_open, A.solution_open, A.maxmarks, 
+     A.negative_marks, A.author_id, A.source, A.author_name, A.view_count  
+     from problem_get_all(p_category:=$1, p_language:=$2, p_sort:=$3, p_author:=$4, p_author_filter:=$5, p_offset:=$6, p_limit:=$7) A `;
 
   pool.query(
     sql,
@@ -507,7 +515,7 @@ exports.searchProblems = async function (req, res, next) {
   });
 
   var sql = `select id, description, solution, options,  
-     answerkey, type, solution_open, author_id, source, rank, author_name, view_count  
+     answerkey, type, solution_open, maxmarks, negative_marks, author_id, source, rank, author_name, view_count  
      from problem_search(p_search_key:= $1, p_author_id:=$2, p_offset:=$3, p_limit:=$4) `;
 
   pool.query(
