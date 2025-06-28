0d155be77df11653f8570de77be0c2dc1d6cfa92
mathew joseph <mathew_joseph31@yahoo.com>
2025-06-28 14:58:24 +0530

    redirect added for ads.txt to ads text manager redirect url

--- setRoutes.js
+++ setRoutes.js
@@ -15,6 +15,10 @@ const seedRedirector = require("./middleware/seedRedirector");
 exports.setRoutes = (app, peerServer) => {
   app.use("/peerjsServer", peerServer);
 
+  app.get("/ads.txt", function (req, res) {
+    res.redirect(constants.ADS_TXT_MANAGER_REDIRECT_URL);
+  });
+
   app.use("/api", apiRouter);
 
   app.use("/", seedRedirector.seedRedirect);
